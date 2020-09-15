756e478c6deb3175290324b5223b371dd58869c9
RodrigoRiveroDS <riverodasilvarodrigo@gmail.com>
2020-09-15 20:40:16 -0300

    creando modelos

---
+++ database/models/comments.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, datatypes) {
+    let alias = "comentario";
+
+    let cols = {
+         id : {
+             type : datatypes.INTEGER,
+             primaryKey: true,
+             autoIncrement: true
+             
+         },
+         order_id : {
+
+         },
+         rating : {
+            type : datatypes.INTEGER,
+
+         },
+         content : {
+            type : datatypes.STRING,
+
+         },
+         created_at : {
+
+         },
+         updated_at : {
+
+         }
+     }
+     let config = {
+         tableName : "comments",
+         timestamps : false
+     }
+
+    let comments = sequelize.define(alias, cols, config);
+
+
+    return comments;
+}
